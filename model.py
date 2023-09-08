83112c92829d2221a7947407acac830e2b1a3d6b
zahra hemmati <98788184+zahra-hematy@users.noreply.github.com>
2023-09-09 03:29:25 +0330

    BatchNorm

--- model.py
+++ model.py
@@ -10,48 +10,87 @@ class AE(torch.nn.Module):
 
         self.encoder = nn.Sequential(
             #Encoder ...
-
             nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
             nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2),
+            
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1),
-            nn.ReLU(),
             nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2),
+            
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
             nn.BatchNorm2d(128),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3,  padding=1),
-            nn.ReLU(),
+            nn.LeakyReLU(0.2),
+            
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3,  stride=2, padding=1),
             nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+           
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
             nn.BatchNorm2d(256),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3,  padding=1),
-            nn.ReLU(),
+            nn.LeakyReLU(0.2),
+            
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3,  stride=2, padding=1),
             nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+            
             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
             nn.BatchNorm2d(512),
-     
-            nn.MaxPool2d(4),
-
+            nn.LeakyReLU(0.2),
+            
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2),
+            
+            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.2),
+            
+            nn.Conv2d(in_channels=1024, out_channels=2048, kernel_size=3,  padding=1),
+            nn.BatchNorm2d(2048),
+            nn.LeakyReLU(0.2),
 
         )
 
         self.decoder = nn.Sequential(
             # Decoder layers...
-           
+            nn.ConvTranspose2d(in_channels=2048, out_channels=1024, stride=1, kernel_size=3, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(),
+            
+            nn.ConvTranspose2d(in_channels=1024, out_channels=512, stride=1, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            
             nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=3, stride=2, padding=1,  output_padding=1),
             nn.BatchNorm2d(256),
+            nn.ReLU(),
+            
             nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.BatchNorm2d(128),
+            nn.ReLU(),
+            
             nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.BatchNorm2d(64),
-            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.BatchNorm2d(32),
+            nn.ReLU(),
+            
             nn.ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(16),
-            nn.ConvTranspose2d(in_channels=16, out_channels=2, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            
+            nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(),
+            
+            nn.ConvTranspose2d(in_channels=8, out_channels=4, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(4),
+            nn.ReLU(),
+            
+            nn.ConvTranspose2d(in_channels=4, out_channels=2, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(2),
 
             nn.Tanh()
